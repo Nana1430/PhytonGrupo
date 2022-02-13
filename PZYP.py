37ca797f7cb53f530cec9fd7d3b9e72868608939
Sonia Jardim <soniajardim.pt@gmail.com>
2022-02-13 14:46:20 +0000

    PZYP.py final

--- PZYP.py
+++ PZYP.py
@@ -10,7 +10,7 @@ def elements_in_array(check_elements, elements):
     i = 0
     offset = 0
     for element in elements:
-        if len(check_elements) <= offset:    # All of the elements in check_elements are in elements
+        if len(check_elements) <= offset:                   # All of the elements in check_elements are in elements
             return i - len(check_elements)
         #:
         if check_elements[offset] == element:
@@ -27,9 +27,9 @@ encoding = "utf-8"
 def encode(text, max_sliding_window_size=4096):
     text_bytes = text.encode(encoding)
 
-    search_buffer = [] # Array of integers, representing bytes
-    check_characters = [] # Array of integers, representing bytes
-    output = [] # Output array
+    search_buffer = []                                      # Array of integers, representing bytes
+    check_characters = []                                   # Array of integers, representing bytes
+    output = []                                             # Output array
 
     i = 0
     for char in text_bytes:
@@ -39,32 +39,32 @@ def encode(text, max_sliding_window_size=4096):
             if i == len(text_bytes) - 1 and elements_in_array(check_characters + [char], search_buffer) != -1:
                 
                 check_characters.append(char)
-		# Only if it's the last character then add the next character to the text the token is representing
+		        # Only if it's the last character then add the next character to the text the token is representing
             #:
             if len(check_characters) > 1:
                 index = elements_in_array(check_characters, search_buffer)
                 offset = i - index - len(check_characters) 	# Calculate the relative offset
-                length = len(check_characters)	 		# Set the length of the token (how many character it represents)
+                length = len(check_characters)	 		    # Set the length of the token (how many character it represents)
 
-                token = f"<{offset},{length}>" 			# Build our token
+                token = f"<{offset},{length}>" 			    # Build our token
 
-                if len(token) > length:  			# Length of token is greater than the length it represents, so output the characters                   
+                if len(token) > length:  			        # Length of token is greater than the length it represents, so output the characters                   
                     output.extend(check_characters) 		# Output the characters
                 else:
                     output.extend(token.encode(encoding)) 	# Output our token
                 #:
                 search_buffer.extend(check_characters) 		# Add the characters to our search buffer   
             else:
-                output.extend(check_characters) 		# Output the character  
+                output.extend(check_characters) 		    # Output the character  
                 search_buffer.extend(check_characters) 		# Add the characters to our search buffer   
-	    #:
+	        #:
             check_characters = []   
         #:
         check_characters.append(char)
 
         if len(search_buffer) > max_sliding_window_size: 	# Check to see if it exceeds the max_sliding_window_size
-            search_buffer = search_buffer[1:] 			# Remove the first element from the search_buffer
-	#:
+            search_buffer = search_buffer[1:] 			    # Remove the first element from the search_buffer
+	    #:
         i += 1
     #:
     return bytes(output)
@@ -74,23 +74,23 @@ encoding = "utf-8"
 #Descompressor
 def decode(text):
 	
-    text_bytes = text.encode(encoding) 				# The text encoded as bytes
-    output = [] 						# The output characters
+    text_bytes = text.encode(encoding) 				        # The text encoded as bytes
+    output = [] 						                    # The output characters
 
     inside_token = False
     scanning_offset = True
 
-    length = [] 						# Length number encoded as bytes
-    offset = [] 						# Offset number encoded as bytes
+    length = [] 						                    # Length number encoded as bytes
+    offset = [] 						                    # Offset number encoded as bytes
 
     for char in text_bytes:
         if char == "<".encode(encoding)[0]:
-            inside_token = True 				# We're now inside a token
-            scanning_offset = True 				# We're now looking for the length number
+            inside_token = True 				            # We're now inside a token
+            scanning_offset = True 				            # We're now looking for the length number
         elif char == ",".encode(encoding)[0] and inside_token:
             scanning_offset = False
         elif char == ">".encode(encoding)[0]:
-            inside_token = False 				# We're no longer inside a token
+            inside_token = False 				            # We're no longer inside a token
 
             # Convert length and offsets to an integer
             length_num = int(bytes(length).decode(encoding))
@@ -99,7 +99,7 @@ def decode(text):
             # Get text that the token represents
             referenced_text = output[-offset_num:][:length_num]
 
-            output.extend(referenced_text) 			# referenced_text is a list of bytes so we use extend to add each one to output
+            output.extend(referenced_text) 			        # referenced_text is a list of bytes so we use extend to add each one to output
 
             # Reset length and offset
             length, offset = [], []
@@ -107,16 +107,16 @@ def decode(text):
         elif inside_token:
             if offset == "":
                 raise ValueError(f'PZYPError.')
-	    #:
+	        #:
             if scanning_offset:
                 offset.append(char)
             else:
                 length.append(char)
-	    #:
+	        #:
         else:
-            output.append(char) # Add the character to our output
-	#:
-    
+            output.append(char)                             # Add the character to our output
+	    #:
+    #:
     return bytes(output)
 #:
 
@@ -128,10 +128,10 @@ def compress_file(fich):
         print("O ficheiro comprimido", fich.rsplit('.', 1)[0]+".LZS", "foi criado !")
         text_File.close()
         compressed_file.close()
-
     else:
         print("O ficheiro especificado", fich, "não existe!")
-
+    #:
+#:    
 def decompress_file(fich):
     if os.path.exists(fich):
         compressed_file = open(fich,"r")
@@ -142,32 +142,29 @@ def decompress_file(fich):
         print("O ficheiro foi descomprimido!")
     else:
         print("O ficheiro especificado", fich, "não existe!")    
-
-
-
+    #:
+#:  
 def sys_argv_func(*args):
     if len(sys.argv) <= 1 or len(sys.argv) >= 4:
         print("Utilização: PYZP", "[-d](para descomprimir - opcional)", 
             "ficheiro", file=sys.stderr)
         sys.exit(2)
     #:
-    if len(sys.argv) == 2: 					# only the file argument (compress)
+    if len(sys.argv) == 2: 					                # only the file argument (compress)
         compress_file(sys.argv[1])
     #:
-    if len(sys.argv) == 3: 					# 2 arguments, testing if is -d
+    if len(sys.argv) == 3: 					                # 2 arguments, testing if is -d
         if sys.argv[1].lower() != "-d":
             print("Argumento desconhecido",sys.argv[1], " Utilização: PYZP", "[-d](para descomprimir - opcional)", 
             "ficheiro", file=sys.stderr)
             sys.exit(2)
         else:
             decompress_file(sys.argv[2])
-	#:
+	    #:
     #:
-
+#:
 if __name__ == "__main__":
-    sys_argv_func(sys.argv)
-    
-    
+    sys_argv_func(sys.argv)   
 #:
 
 
